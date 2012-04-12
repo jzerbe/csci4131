ce9046898bd9164b1068b701a4456434a320cf82
Jason Zerbe <jzerbe@vraidsys.com>
2012-04-12 10:52:59 -0500

    added skeleton proj5

---
+++ proj5/README.txt
@@ -0,0 +1,10 @@
+Jason Zerbe
+3830775
+
+Java Servlet RSS feed filtering script
+
+setup on CSELabs webserver
+1) unpack WAR if not using Tomcat deployment interface and upload unpacked contents
+2) chmod 644 all files and directories
+3) access with web-browser --> /class/tomcat/zerbe006 translates
+to http://tomcat.cselabs.umn.edu:8081/zerbe006/
---
+++ proj5/src/servlets/Feeds.java
@@ -0,0 +1,20 @@
+/*
+ * GET - filtered RSS feed information output as XHTML
+ */
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Jason Zerbe
+ */
+public class Feeds extends HttpServlet {
+
+    private static final long serialVersionUID = 42L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    }
+}
